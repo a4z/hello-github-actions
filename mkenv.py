d5f40250bdfad452cad4e15e429c2b670f9cbb81
a4z <harald.achitz@gmail.com>
2022-10-13 09:04:33 +0200

    Dyn var access

--- mkenv.py
+++ mkenv.py
@@ -5,5 +5,5 @@ gh_action_env = os.environ.get('GITHUB_ENV', 'n/a')
 
 
 with open(gh_action_env, 'a') as f:
-    f.write(f"a_val=VALUE-A\n")
-    f.write(f"b_val=VALUE-B\n")
+    f.write(f"first=VALUE-A\n")
+    f.write(f"second=VALUE-B\n")
